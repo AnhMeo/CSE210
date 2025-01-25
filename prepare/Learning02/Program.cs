8217330856296ebd5409e7e2f6da7a040e1e7d32
AnhMeo <u799696294@byui.edu>
2025-01-26 02:17:31 +0700

    Completed the exercise and finally submitted it! Wooo

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Apple Inc.";
+        job1._jobTitle = "Software Developer";
+        job1._startYear = 2025;
+        job1._endYear = 2026;
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._company  = "Microsoft";
+        job2._jobTitle = "Cybersecurity Technician";
+        job2._startYear = 2020;
+        job2._endYear = 2025;
+        job2.Display();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Christian Taylor";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
+        
     }
 }
